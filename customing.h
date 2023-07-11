38449cf046532f9470e9fab9e53ff93ce95aadf7
TemplateKing <rodrigofisher@yahoo.com>
2023-07-11 13:01:35 +0300

    Added Files

---
+++ customing.h
@@ -0,0 +1,13 @@
+#include <string>
+#include "corefunc.h"
+#define byte uint8_t
+#define BYTE uint8_t
+
+//Customing GrowID
+string Growid_acc = "JzuvNiAh";
+
+//Custome Password By JzuvGTI
+string Pass_acc = "Jembutan@22";
+
+//Urutan Nomer ID
+int Urutan_ID = 1;
